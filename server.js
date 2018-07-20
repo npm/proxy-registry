cc8f4d9d4457b6e347301ae6adbd35316d38446f
Rebecca Turner <me@re-becca.org>
2018-07-19 17:07:11 -0700

    Add cli options

--- server.js
+++ server.js
@@ -1,7 +1,26 @@
+#!/usr/bin/env node
 'use strict'
+require('@iarna/cli')(main)
+  .option('port', {
+    describe: 'the port to listen on',
+    type: 'number',
+    default: 22000
+  })
+  .option('shell', {
+    describe: 'run a shell configured to talk to this proxy',
+    type: 'boolean',
+    default: true
+  })
+  .option('log', {
+    describe: 'log requests (defaults to off when running a shell, on when not)',
+    type: 'boolean'
+  })
+
+const spawn = require('child_process').spawn
 const pacote = require('pacote')
 const fetch = require('make-fetch-happen')
 const qr = require('@perl/qr')
+const http = require('http');
 
 const Koa = require('koa')
 const compress = require('koa-compress')
@@ -16,6 +35,39 @@ const matchName = qr`(?:@[^/+]/)?[^/]+`
 const matchVersion = qr`\d+\.\d+\.\d+(?:-.*)?`
 const matchTarball = qr`^/(${matchName})/-/.*?-(${matchVersion})\.tgz$`
 const matchManifest = qr`^/(${matchName})$`
+
+const isWindows = process.platform === 'win32'
+const isWindowsBash = (/^MINGW(32|64)$/.test(process.env.MSYSTEM) || process.env.TERM === 'cygwin')
+const isWindowsShell = isWindows && !isWindowsBash
+
+async function main (opts, ...args) {
+  if (opts.log == null) opts.log = !opts.shell
+  const app = new Koa()
+  if (opts.log) app.use(logger())
+  app.use(alwaysJson())
+  app.use(compress())
+  app.use(handleRequest)
+  const srv = http.createServer(app.callback()).listen(opts.port);
+
+  await new Promise((resolve, reject) => {
+    app.on('error', reject)
+    srv.on('error', reject)
+    if (opts.shell) {
+      process.env['npm_config_registry'] = `http://localhost:${opts.port}`
+      console.log(`Starting subshell configured to talk to: https://localhost:${opts.port}`)
+      console.log(`To close server, run: exit`)
+      spawn(conf.npm.shell, [], {stdio: 'inherit'})
+        .on('close', er => er ? reject(er) : resolve())
+    } else {
+      console.log(`Listening on: https://localhost:${opts.port}`)
+      console.log(`To use: npm config set registry https://localhost:${opts.port}`)
+      console.log(`^C to close server`)
+      process.on('SIGINT', resolve)
+    }
+  })
+  console.error('\nShutting down')
+  srv.close()
+}
 
 async function handleRequest (ctx, next) {
   const requestConfig = Object.assign({}, conf.pacote, {headers: ctx.request.header})
@@ -62,10 +114,3 @@ async function proxyRequest (url, ctx, requestConfig) {
   return result
 }
 
-const app = new Koa()
-app.use(logger())
-app.use(alwaysJson())
-app.use(compress())
-app.use(handleRequest)
-console.log('Listening on', 22000)
-app.listen(22000)
